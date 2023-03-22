18d70c87036ca0fdc724cba10a9ce76eb02bac43
kehanbo <875225214@qq.com>
2023-03-23 00:36:41 +0800

    添加头部文件

---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">dfsdfsad</div>
+</template>
+<script>
+export default {
+  name: 'm-header'
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 40px;
+  text-align: center;
+}
+</style>
